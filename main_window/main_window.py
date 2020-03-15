04d890bf21cac212f181c5600cb0ea21068ef980
bullfroggie <lausevnikola@protonmail.com>
2020-03-15 16:45:46 +0100

    Add __init__.py file

--- main_window/main_window.py
+++ main_window/main_window.py
@@ -1,8 +1,8 @@
 from PySide2.QtWidgets import QMainWindow
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("Zephyrus - Information Handler")
 
-        

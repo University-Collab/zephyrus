a036ac58b36b80e834a7f1fc7682ee47166fa41a
bullfroggie <lausevnikola@protonmail.com>
2020-04-08 11:22:36 +0200

    Add checking of which platform is Zephyrus run on and apply the corresponding style

--- view/application/application.py
+++ view/application/application.py
@@ -1,3 +1,4 @@
+from sys import platform
 from PySide2.QtWidgets import QApplication
 
 
@@ -5,8 +6,12 @@ class Application(QApplication):
     def __init__(self, argv):
         super().__init__(argv)
 
-        with open("view/style_file.qss", "r") as qss_file:
-            self.setStyleSheet(qss_file.read())
+        if platform == "linux" or platform == "darwin":
+            with open("view/style_linux.qss", "r") as qss_file:
+                self.setStyleSheet(qss_file.read())
+        else:
+            # add style for Windows
+            pass
 
     def exec(self):
         super().exec_()

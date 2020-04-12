36c157a7312c72aef6db5d159e4d6ceb3580fc7c
bullfroggie <lausevnikola@protonmail.com>
2020-04-12 19:41:36 +0200

    Add window icon

--- view/main_window/main_window.py
+++ view/main_window/main_window.py
@@ -1,7 +1,9 @@
 from PySide2.QtWidgets import QMainWindow
+from PySide2.QtGui import QIcon
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("Zephyrus - Information Handler")
+        self.setWindowIcon(QIcon("view/images/branding/zephyrus_transparent.png"))

5911555f81ffe27562d3d7f1318b1f0141b2a60f
bullfroggie <lausevnikola@protonmail.com>
2020-04-12 20:55:36 +0200

    Add shortcuts for Tool Bar and Dock Show/Hide and tweaked style

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ if __name__ == "__main__":
 
     menubar = Menubar(window)
     menubar.theme_menu.addAction("Dark", app.set_dark_theme)
+    menubar.theme_menu.addSeparator()
+    menubar.view_menu.addSeparator()
     menubar.theme_menu.addAction("Light", app.set_light_theme)
     window.setMenuBar(menubar)
 
-    toolbar = Toolbar(window)
+    toolbar = Toolbar("Tool Bar", window)
+    toggle_toolbar_action = toolbar.toggleViewAction()
+    toggle_toolbar_action.setShortcut("Ctrl+T")
+    menubar.view_menu.addAction(toggle_toolbar_action)
     window.addToolBar(Qt.LeftToolBarArea, toolbar)
 
     central_widget = CentralWidget(window)
@@ -34,6 +39,7 @@ if __name__ == "__main__":
     dock = Dock("File Explorer", central_widget, window)
     window.addDockWidget(Qt.LeftDockWidgetArea, dock)
     toggle_dock_action = dock.toggleViewAction()
+    toggle_dock_action.setShortcut("Ctrl+B")
     menubar.view_menu.addAction(toggle_dock_action)
 
     window.showMaximized()

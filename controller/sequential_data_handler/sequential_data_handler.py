25581dd09291741c7987dbb0470930faf8be82be
bullfroggie <lausevnikola@protonmail.com>
2020-04-20 17:10:16 +0200

    Implement data serialization using pickle instead of json

--- controller/sequential_data_handler/sequential_data_handler.py
+++ controller/sequential_data_handler/sequential_data_handler.py
@@ -1,9 +1,13 @@
-import json, bisect
+import json, pickle, bisect
 from controller.data_handler.data_handler import DataHandler
 
 
 class SequentialDataHandler(DataHandler):
-    def __init__(self, path, meta_path, is_parent_table=True, unique_data=None):
+    def __init__(self,
+                 path,
+                 meta_path,
+                 is_parent_table=True,
+                 unique_data=None):
         super().__init__()
         self.path = path
         self.meta_path = meta_path
@@ -17,18 +21,22 @@ class SequentialDataHandler(DataHandler):
         self.load_data()
 
     def load_data(self):
-        with open(self.meta_path) as data_file:
+        with open(self.meta_path, "r") as data_file:
             self.meta_data = json.load(data_file)
             self.search_key = self.meta_data["search key"]
-            self.linked_file_path = (self.path.split("storage/")[0] + "storage/" + self.meta_data["linked file"])
+            self.linked_file_path = (self.path.split("storage/")[0] +
+                                     "storage/" +
+                                     self.meta_data["linked file"])
 
         if self.is_parent_table == True:
-            with open(self.path) as data_file:
-                self.data = sorted(json.load(data_file), key=lambda k: k[self.search_key])
+            with open(self.path, "rb") as data_file:
+                self.data = sorted(pickle.load(data_file),
+                                   key=lambda k: k[self.search_key])
 
         if self.is_parent_table == False:
-            with open(self.linked_file_path) as data_file:
-                self.loaded_data = sorted(json.load(data_file), key=lambda k: k[self.search_key])
+            with open(self.linked_file_path, "rb") as data_file:
+                self.loaded_data = sorted(pickle.load(data_file),
+                                          key=lambda k: k[self.search_key])
                 for obj in self.loaded_data:
                     if obj[self.search_key] == self.unique_data:
                         self.data.append(obj)
@@ -54,11 +62,11 @@ class SequentialDataHandler(DataHandler):
 
     def save(self, data, parent_table=False, sub_table=False):
         if parent_table and not sub_table:
-            with open(self.path, "w") as json_file:
-                json.dump(data, json_file, indent=4)
+            with open(self.path, "wb") as pickle_file:
+                pickle.dump(data, pickle_file)
         elif sub_table and not parent_table:
-            with open(self.linked_file_path, "w") as json_file:
-                json.dump(data, json_file, indent=4)
+            with open(self.linked_file_path, "wb") as pickle_file:
+                pickle.dump(data, pickle_file)
 
     def edit(self, obj):
         if self.is_parent_table:
@@ -80,13 +88,15 @@ class SequentialDataHandler(DataHandler):
             for updated_obj in self.data:
                 data.append(updated_obj)
 
-            self.save(data=sorted(data, key=lambda k: k[self.search_key]), sub_table=True)
+            self.save(data=sorted(data, key=lambda k: k[self.search_key]),
+                      sub_table=True)
 
     def edit_subtable_unique_data(self, old_value, new_value):
         subtable_data = []
 
-        with open(self.linked_file_path) as data_file:
-            subtable_data = sorted(json.load(data_file), key=lambda k: k[self.search_key])
+        with open(self.linked_file_path, "rb") as data_file:
+            subtable_data = sorted(pickle.load(data_file),
+                                   key=lambda k: k[self.search_key])
 
         for d in subtable_data:
             if d[self.meta_data["search key"]] == old_value:
--- controller/serial_data_handler/serial_data_handler.py
+++ controller/serial_data_handler/serial_data_handler.py
@@ -1,9 +1,13 @@
-import json
+import json, pickle
 from controller.data_handler.data_handler import DataHandler
 
 
 class SerialDataHandler(DataHandler):
-    def __init__(self, path, meta_path, is_parent_table=True, unique_data=None):
+    def __init__(self,
+                 path,
+                 meta_path,
+                 is_parent_table=True,
+                 unique_data=None):
         super().__init__()
         self.path = path
         self.meta_path = meta_path
@@ -17,18 +21,20 @@ class SerialDataHandler(DataHandler):
         self.load_data()
 
     def load_data(self):
-        with open(self.meta_path) as data_file:
+        with open(self.meta_path, "r") as data_file:
             self.meta_data = json.load(data_file)
             self.search_key = self.meta_data["search key"]
-            self.linked_file_path = (self.path.split("storage/")[0] + "storage/" + self.meta_data["linked file"])
+            self.linked_file_path = (self.path.split("storage/")[0] +
+                                     "storage/" +
+                                     self.meta_data["linked file"])
 
         if self.is_parent_table == True:
-            with open(self.path) as data_file:
-                self.data = json.load(data_file)
+            with open(self.path, "rb") as data_file:
+                self.data = pickle.load(data_file)
 
         if self.is_parent_table == False:
-            with open(self.linked_file_path) as data_file:
-                self.loaded_data = json.load(data_file)
+            with open(self.linked_file_path, "rb") as data_file:
+                self.loaded_data = pickle.load(data_file)
                 for obj in self.loaded_data:
                     if obj[self.search_key] == self.unique_data:
                         self.data.append(obj)
@@ -48,24 +54,14 @@ class SerialDataHandler(DataHandler):
 
     def save(self, data, parent_table=False, sub_table=False):
         if parent_table and not sub_table:
-            with open(self.path, "w") as json_file:
-                json.dump(data, json_file, indent=4)
+            with open(self.path, "wb") as pickle_file:
+                pickle.dump(data, pickle_file)
         elif sub_table and not parent_table:
-            with open(self.linked_file_path, "w") as json_file:
-                json.dump(data, json_file, indent=4)
+            with open(self.linked_file_path, "wb") as pickle_file:
+                pickle.dump(data, pickle_file)
 
     def edit(self, obj):
         if self.is_parent_table:
-            """
-            DELETE IF NOT NEEDED
-            """
-            # for d in self.data:
-            #     if getattr(d, self.search_key) == getattr(obj, self.search_key):
-            #         for key in self.meta_data["columns"]:
-            #             d[key] = obj[key]
-            #         self.save(self.data, parent_table=True)
-            #         return
-            # return
             self.save(self.data, parent_table=True)
         else:
             data = []
@@ -87,14 +83,13 @@ class SerialDataHandler(DataHandler):
 
     def edit_subtable_unique_data(self, old_value, new_value):
         subtable_data = []
-        with open(self.linked_file_path) as data_file:
-            subtable_data = json.load(data_file)
+        with open(self.linked_file_path, "rb") as data_file:
+            subtable_data = pickle.load(data_file)
         for d in subtable_data:
             if d[self.meta_data["search key"]] == old_value:
                 d[self.meta_data["search key"]] = new_value
 
-        with open(self.linked_file_path, "w") as json_file:
-            json.dump(subtable_data, json_file, indent=4)
+        self.save(data=subtable_data, sub_table=True)
 
     def delete_one(self, unique_data):
         position = 0

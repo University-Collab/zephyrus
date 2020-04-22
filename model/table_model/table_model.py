5fe90bd39c08fb4aec2113f3a377fd9463922081
bullfroggie <lausevnikola@protonmail.com>
2020-04-22 19:41:14 +0200

    Fix issue where two identical entities were added when editing

--- model/table_model/table_model.py
+++ model/table_model/table_model.py
@@ -43,14 +43,12 @@ class TableModel(QAbstractTableModel):
         i = 0
         if self.is_parent_table == True:
             for i in range(len(self.metadata["columns"])):
-                if (section == i and orientation == QtCore.Qt.Horizontal
-                        and role == QtCore.Qt.DisplayRole):
+                if (section == i and orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole):
                     return self.metadata["columns"][i]
 
         if self.is_parent_table == False:
             for i in range(len(self.metadata["subtable columns"])):
-                if (section == i and orientation == QtCore.Qt.Horizontal
-                        and role == QtCore.Qt.DisplayRole):
+                if (section == i and orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole):
                     return self.metadata["subtable columns"][i]
 
         return None
@@ -58,18 +56,18 @@ class TableModel(QAbstractTableModel):
     # Editable model methods
     def setData(self, index, value, role=QtCore.Qt.EditRole):
         data = self.get_element(index)
+
         i = 0
+
         if value == "":
             return False
 
         if self.is_parent_table == True:
             for i in range(len(self.metadata["columns"])):
                 if index.column() == i and role == QtCore.Qt.EditRole:
-                    if self.metadata["columns"][i] == self.metadata[
-                            "search key"]:
+                    if self.metadata["columns"][i] == self.metadata["search key"]:
                         old_value = data[self.metadata["columns"][i]]
-                        self.handler_reference.edit_subtable_unique_data(
-                            old_value, value)
+                        self.handler_reference.edit_subtable_unique_data(old_value, value)
 
                     data[self.metadata["columns"][i]] = value
                     self.handler_reference.edit(data)
@@ -78,6 +76,10 @@ class TableModel(QAbstractTableModel):
         if self.is_parent_table == False:
             for i in range(len(self.metadata["subtable columns"])):
                 if index.column() == i and role == QtCore.Qt.EditRole:
+                    if self.metadata["subtable columns"][i] == self.metadata["search key"]:
+                        old_value = data[self.metadata["subtable columns"][i]]
+                        self.handler_reference.edit_subtable_unique_data(old_value, value)
+
                     data[self.metadata["subtable columns"][i]] = value
                     self.handler_reference.edit(data)
                     return True

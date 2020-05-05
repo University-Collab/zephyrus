a3dbc749aa0c86f2fb6c6a315db3765ea70ec43a
bullfroggie <lausevnikola@protonmail.com>
2020-05-05 17:49:56 +0200

    Tweak style and add tool tips

--- view/toolbar/toolbar.py
+++ view/toolbar/toolbar.py
@@ -9,25 +9,31 @@ class Toolbar(QToolBar):
 
     def init_toolbar_buttons(self):
         self.create_new = QToolButton(self)
-        self.create_new.setIcon(QIcon("view/images/toolbar/create_new.png"))
+        self.create_new.setIcon(QIcon("view/images/toolbar/new.png"))
+        self.create_new.setToolTip("Create New")
         self.addWidget(self.create_new)
 
         self.open = QToolButton(self)
         self.open.setIcon(QIcon("view/images/toolbar/open.png"))
+        self.open.setToolTip("Open")
         self.addWidget(self.open)
 
         self.save = QToolButton(self)
         self.save.setIcon(QIcon("view/images/toolbar/save.png"))
+        self.save.setToolTip("Save")
         self.addWidget(self.save)
 
         self.search = QToolButton(self)
         self.search.setIcon(QIcon("view/images/toolbar/search.png"))
+        self.search.setToolTip("Search")
         self.addWidget(self.search)
 
         self.undo = QToolButton(self)
         self.undo.setIcon(QIcon("view/images/toolbar/undo.png"))
+        self.undo.setToolTip("Undo Changes")
         self.addWidget(self.undo)
 
         self.redo = QToolButton(self)
         self.redo.setIcon(QIcon("view/images/toolbar/redo.png"))
+        self.redo.setToolTip("Redo Changes")
         self.addWidget(self.redo)

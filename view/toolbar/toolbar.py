b0275ce09072a3913d8176f8dc7a3d01ad0f7b60
Nikola Lausev <lausevnikola@tutanota.de>
2020-06-17 21:10:44 +0200

    Implement action for initiating database connection dialog

--- view/toolbar/toolbar.py
+++ view/toolbar/toolbar.py
@@ -1,16 +1,19 @@
 from PySide2.QtWidgets import QToolBar, QWidget, QToolButton
 from PySide2.QtGui import QIcon
+from view.connect_database.connect_database import ConnectDatabase
 
 
 class Toolbar(QToolBar):
     def __init__(self, title=None, parent=None):
         QToolBar.__init__(self, title, parent)
+        self.connect_db = ConnectDatabase(parent)
         self.init_toolbar_buttons()
 
     def init_toolbar_buttons(self):
         self.connect_database = QToolButton(self)
         self.connect_database.setIcon(QIcon("view/images/toolbar/database-connect.png"))
         self.connect_database.setToolTip("Connect Database")
+        self.connect_database.clicked.connect(self.connect_db.display_dialog)
         self.addWidget(self.connect_database)
 
         self.open = QToolButton(self)
